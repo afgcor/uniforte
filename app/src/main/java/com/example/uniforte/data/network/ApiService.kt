636da229c13f7a01d787689e299589acfd1001d0
Lais Barbosa <laissilva@edu.unifor.br>
2025-06-02 16:12:20 -0300

    implementação de edição e exibição de dados na tela de perfil

--- app/src/main/java/com/example/uniforte/data/network/ApiService.kt
+++ app/src/main/java/com/example/uniforte/data/network/ApiService.kt
@@ -1,17 +1,32 @@
-package com.example.uniforte.data.network // Ajuste o pacote
+package com.example.uniforte.data.network
 
-import okhttp3.ResponseBody // Importar ResponseBody
+import com.example.uniforte.data.network.UpdateUsuarioRequest
+import com.example.uniforte.data.network.Usuario
+import okhttp3.ResponseBody
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.Call
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiService {
 
-    @POST("api/usuarios/login" ) // Endpoint relativo à BASE_URL
+    @POST("api/usuarios/login")
     suspend fun login(@Body requestBody: Map<String, String>): Response<ResponseBody> // Usar Map e ResponseBody
 
-
     @POST("api/usuarios/")
-    fun cadastrarUsuario(@Body requestBody: Map<String, String>): Call <ResponseBody> //Retorna Call
+    fun cadastrarUsuario(@Body requestBody: Map<String, String>): Call<ResponseBody> // Retorna Call
+
+    @GET("api/usuarios/{id}")
+    suspend fun buscarUsuarioPorId(@Path("id") userId: String): Response<Usuario>
+
+    @PUT("api/usuarios/{id}")
+    suspend fun atualizarUsuario(
+        @Path("id") userId: String,
+        @Body dadosAtualizacao: UpdateUsuarioRequest
+    ): Response<Usuario>
+
 }
+
